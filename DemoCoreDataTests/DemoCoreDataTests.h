2c8696d3434bf400f59826e701d313c3576e1800
Mr Tuan <mrtuan@Mr-Tuan.local>
2013-02-25 14:48:54 +0700

    Initial Commit

---
+++ DemoCoreDataTests/DemoCoreDataTests.h
@@ -0,0 +1,13 @@
+//
+//  DemoCoreDataTests.h
+//  DemoCoreDataTests
+//
+//  Created by Mr Tuan on 2/25/13.
+//  Copyright (c) 2013 Mr Tuan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DemoCoreDataTests : SenTestCase
+
+@end
---
+++ DemoCoreDataTests/DemoCoreDataTests.m
@@ -0,0 +1,32 @@
+//
+//  DemoCoreDataTests.m
+//  DemoCoreDataTests
+//
+//  Created by Mr Tuan on 2/25/13.
+//  Copyright (c) 2013 Mr Tuan. All rights reserved.
+//
+
+#import "DemoCoreDataTests.h"
+
+@implementation DemoCoreDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DemoCoreDataTests");
+}
+
+@end
